1a48e57a65c22230dc775044662be3c5274ef34a
Michael Demarco <miczdem@gmail.com>
2016-11-27 09:09:11 +1100

    step 7

--- app/app.component.ts
+++ app/app.component.ts
@@ -71,9 +71,20 @@ const HEROES: Hero[] = [
   `],
   template: `
   <h1>{{title}}</h1>
+
+  <div *ngIf="selectedHero">
+    <h2>{{selectedHero.name}} details!</h2>
+    <div><label>id: </label>{{selectedHero.id}}</div>
+    <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+    </div>
+  </div>
+
   <h2>My Heroes</h2>
   <ul class="heroes">
-    <li *ngFor="let hero of heroes">
+    <li *ngFor="let hero of heroes" (click)="onSelect(hero)"
+      [class.selected]="hero === selectedHero">
       <span class="badge">{{hero.id}}</span> {{hero.name}}  
     </li>
   </ul>
@@ -82,4 +93,9 @@ const HEROES: Hero[] = [
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
