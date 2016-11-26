a8c76c3c814d41dff8543763db58276817322d8c
Michael Demarco <miczdem@gmail.com>
2016-11-27 09:21:42 +1100

    step 8

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -72,14 +68,7 @@ const HEROES: Hero[] = [
   template: `
   <h1>{{title}}</h1>
 
-  <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-    </div>
-  </div>
+  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
 
   <h2>My Heroes</h2>
   <ul class="heroes">
