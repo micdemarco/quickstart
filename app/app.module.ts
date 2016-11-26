3b9d1258a93ad0bda8efaa7738475b4989d77096
Michael Demarco <miczdem@gmail.com>
2016-11-27 09:40:32 +1100

    step 9

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers:    [ HeroService ]
 })
-export class AppModule { }
+export class AppModule {
+}
